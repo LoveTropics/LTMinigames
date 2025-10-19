3728e5533c926dd4855762cbb64c06898e5d31ec
Gegy <me@gegy.dev>
2025-10-19 19:45:17 +0200

    Add `max_count` field to `permanent_items` to allow adding items over time

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/PermanentItemBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/PermanentItemBehavior.java
@@ -10,10 +10,13 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.world.item.ItemStack;
 
-public record PermanentItemBehavior(ItemStack item, int interval) implements IGameBehavior {
+import java.util.Optional;
+
+public record PermanentItemBehavior(ItemStack item, int interval, Optional<Integer> maxCount) implements IGameBehavior {
 	public static final MapCodec<PermanentItemBehavior> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
 			MoreCodecs.ITEM_STACK.fieldOf("item").forGetter(c -> c.item),
-			Codec.INT.optionalFieldOf("interval", 5).forGetter(c -> c.interval)
+			Codec.INT.optionalFieldOf("interval", 5).forGetter(c -> c.interval),
+			Codec.INT.optionalFieldOf("max_count").forGetter(c -> c.maxCount)
 	).apply(i, PermanentItemBehavior::new));
 
 	@Override
@@ -21,9 +24,10 @@ public record PermanentItemBehavior(ItemStack item, int interval) implements IGa
 		events.listen(GamePlayerEvents.TICK, player -> {
 			if (game.participants().contains(player) && game.ticks() % interval == 0) {
 				int currentCount = player.getInventory().countItem(item.getItem());
-				int targetCount = item.getCount();
+				int targetCount = maxCount.orElse(item.getCount());
 				if (currentCount < targetCount) {
-					player.getInventory().add(item.copyWithCount(targetCount - currentCount));
+					int dropCount = Math.min(targetCount - currentCount, item.getCount());
+					player.getInventory().add(item.copyWithCount(dropCount));
 				}
 			}
 		});
