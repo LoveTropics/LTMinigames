2721e2662e8028953283215eaaabf466494f535c
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2025-10-25 18:05:02 -0500

    Hopefully fix locator bar gamerule

---
+++ src/main/java/com/lovetropics/minigames/mixin/ServerwaypointManagerMixin.java
@@ -0,0 +1,18 @@
+package com.lovetropics.minigames.mixin;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.waypoints.ServerWaypointManager;
+import net.minecraft.world.level.GameRules;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ServerWaypointManager.class)
+public class ServerwaypointManagerMixin {
+
+	@Inject(method = "isLocatorBarEnabledFor", at = @At("HEAD"), cancellable = true)
+	private static void locatorBarEnabledFor(ServerPlayer player, CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(player.level().getGameRules().getBoolean(GameRules.RULE_LOCATOR_BAR));
+	}
+}
