339f2b6c6d79208f1e56824803e7137ee5dfe185
Cory Scheviak <cojomax99@gmail.com>
2025-11-15 21:27:06 +0100

    Add upset stomach item icon

--- src/main/java/com/lovetropics/minigames/common/core/item/MinigameItems.java
+++ src/main/java/com/lovetropics/minigames/common/core/item/MinigameItems.java
@@ -16,6 +16,7 @@ public class MinigameItems {
 	public static final ItemEntry<Item> UPSET_STOMACH = REGISTRATE.item("upset_stomach", Item::new).register();
 	public static final ItemEntry<Item> COLADARAL_DAMAGE = REGISTRATE.item("coladaral_damage", Item::new).register();
 	public static final ItemEntry<Item> TAPIR_TAKEOVER = REGISTRATE.item("tapir_takeover", Item::new).register();
+
 	/** End powerups */
 
 	public static void init() {
