d7676a1e0ab844ec4496f9ac13929e6f8bcb197a
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2025-11-15 21:03:34 -0600

    Add player predicate to OnDamgeTrigger

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/trigger/OnDamageTrigger.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/trigger/OnDamageTrigger.java
@@ -8,18 +8,28 @@ import com.lovetropics.minigames.common.core.game.behavior.action.GameActionList
 import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
 import com.lovetropics.minigames.common.core.game.behavior.event.GamePlayerEvents;
 import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.advancements.critereon.EntityPredicate;
 import net.minecraft.util.TriState;
 import net.minecraft.util.context.ContextMap;
 
-public record OnDamageTrigger(GameActionList actions) implements IGameBehavior {
-	public static final MapCodec<OnDamageTrigger> CODEC = GameActionList.MAP_CODEC.xmap(OnDamageTrigger::new, OnDamageTrigger::actions);
+import java.util.Optional;
+
+public record OnDamageTrigger(GameActionList actions, Optional<EntityPredicate> predicate) implements IGameBehavior {
+
+	public static final MapCodec<OnDamageTrigger> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+			GameActionList.MAP_CODEC.forGetter(OnDamageTrigger::actions),
+			EntityPredicate.CODEC.optionalFieldOf("predicate").forGetter(OnDamageTrigger::predicate)
+	).apply(instance, OnDamageTrigger::new));
 
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) throws GameException {
 		actions.register(game, events);
 
 		events.listen(GamePlayerEvents.DAMAGE, (player, damageSource, amount) -> {
-			actions.apply(game, ContextMap.EMPTY, ActionSubjects.ofPlayer(player));
+			if (predicate.map(predicate -> predicate.matches(player, player)).orElse(true)) {
+				actions.apply(game, ContextMap.EMPTY, ActionSubjects.ofPlayer(player));
+			}
 			return TriState.DEFAULT;
 		});
 	}
