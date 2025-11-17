5ba17614f53956df83bf99e1c2d8458a1a15bad5
Gegy <me@gegy.dev>
2025-11-17 19:12:02 +0100

    Fix room entrance text rendering

--- src/main/java/com/lovetropics/minigames/common/content/escape_race/misc/RoomEntrancePadEntityRenderer.java
+++ src/main/java/com/lovetropics/minigames/common/content/escape_race/misc/RoomEntrancePadEntityRenderer.java
@@ -12,7 +12,6 @@ import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiSpriteManager;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.debug.DebugRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -20,7 +19,8 @@ import net.minecraft.client.renderer.item.ItemModelResolver;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.ARGB;
-import net.minecraft.world.entity.Display;
+import net.minecraft.util.CommonColors;
+import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import org.joml.Quaternionf;
@@ -88,55 +88,47 @@ public class RoomEntrancePadEntityRenderer extends EntityRenderer<RoomEntrancePa
 		renderFace(packedLight, buffer, last, renderState.color, maxX, minY, maxZ, minX, maxY, maxZ, renderState.ticks);
 		renderFace(packedLight, buffer, last, renderState.color, maxX, minY, maxZ, maxX, maxY, minZ, renderState.ticks);
 		poseStack.popPose();
-		int j = (int)(Minecraft.getInstance().options.getBackgroundOpacity(0.25F) * 255.0F) << 24;
-		if(renderState.roomStatus == RoomStatus.LOCKED){
-			poseStack.mulPose(Axis.YP.rotationDegrees(180)); // Facing
-			poseStack.mulPose(this.calculateOrientation(new Quaternionf()));
-			poseStack.translate(-0.4f,  0.5, 0f);
+
+		int backgroundColor = ARGB.color(Minecraft.getInstance().options.getBackgroundOpacity(0.25F), CommonColors.BLACK);
+		poseStack.mulPose(Axis.YP.rotationDegrees(180)); // Facing
+		poseStack.translate(0.0f, 0.5f, 0.0f);
+		poseStack.mulPose(Mth.rotationAroundAxis(Mth.Y_AXIS, entityRenderDispatcher.camera.rotation(), new Quaternionf()));
+
+		if (renderState.roomStatus == RoomStatus.LOCKED) {
+			float textScale = 0.05f;
+			String costText = renderState.cost + "x";
+			int costTextWidth = font.width(costText);
+
+			float buckWidth = 0.7f;
+			float totalWidth = buckWidth + costTextWidth * textScale;
+
 			poseStack.pushPose();
-			poseStack.translate(-0.1f,  0.1f, 0f);
+			poseStack.translate(-totalWidth / 2.0f + buckWidth / 2.0f, 0.0f, 0.0f);
 			renderState.breakBuck.render(poseStack, bufferSource, packedLight, OverlayTexture.NO_OVERLAY);
-			poseStack.popPose();
-			String text = renderState.cost + "x";
-			float xOffset = -font.width(text) / 2f;
-			poseStack.pushPose();
-			poseStack.translate(0.8f,  .3, 0f);
-			poseStack.mulPose(Axis.ZP.rotationDegrees(-180f)); // Turn upsidedown
-			poseStack.scale(0.05f, 0.05f, 0.05f);
+
+			poseStack.translate(buckWidth, 0.0f, 0.0f);
+			poseStack.scale(textScale, -textScale, textScale);
 			font.drawInBatch(
-					text,
-					xOffset - 2f,
-					0f,
-					-1, false, poseStack.last().pose(), bufferSource, Font.DisplayMode.SEE_THROUGH, j, packedLight
+					costText,
+					0.0f,
+					-font.lineHeight / 2.0f,
+					CommonColors.WHITE, false, poseStack.last().pose(), bufferSource, Font.DisplayMode.NORMAL, backgroundColor, packedLight
 			);
 			poseStack.popPose();
 		}
+
 		poseStack.pushPose();
-		poseStack.translate(0.4f,  1.5, 0f);
-		poseStack.mulPose(Axis.ZP.rotationDegrees(-180f)); // Turn upsidedown
-		poseStack.scale(0.08f, 0.08f, 0.08f);
-		float nameXOffset = -font.width(renderState.roomName) / 2f;
+		poseStack.translate(0.0f, 1.5, 0.0f);
+		poseStack.scale(0.08f, -0.08f, 0.08f);
 		font.drawInBatch(
 				renderState.roomName,
-				nameXOffset,
-				0f,
-				-1, false, poseStack.last().pose(), bufferSource, Font.DisplayMode.SEE_THROUGH, j, packedLight
+				-font.width(renderState.roomName) / 2.0f,
+				0.0f,
+				CommonColors.WHITE, false, poseStack.last().pose(), bufferSource, Font.DisplayMode.NORMAL, backgroundColor, packedLight
 		);
 		poseStack.popPose();
+
 		poseStack.popPose();
-	}
-
-	private Quaternionf calculateOrientation(Quaternionf quaternion) {
-		Camera camera = this.entityRenderDispatcher.camera;
-		return quaternion.rotationYXZ((float) (-Math.PI / 180.0) * cameraYrot(camera), (float) (-Math.PI / 180.0) * cameraXRot(camera), 0.0F);
-	}
-
-	private static float cameraYrot(Camera camera) {
-		return camera.getYRot() - 180.0F;
-	}
-
-	private static float cameraXRot(Camera camera) {
-		return -camera.getXRot();
 	}
 
 	private static void renderFace(int packedLight, VertexConsumer buffer, PoseStack.Pose last, int color, float minX, float minY, float minZ, float maxX, float maxY, float maxZ, int ticks) {
