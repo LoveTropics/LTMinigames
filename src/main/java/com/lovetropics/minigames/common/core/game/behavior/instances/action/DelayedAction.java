f924c06443afb9c267bb9b4b45c191b3f30d94c0
Gegy <me@gegy.dev>
2025-11-20 11:47:29 +0100

    Allow repetitions for delayed action

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/action/DelayedAction.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/action/DelayedAction.java
@@ -2,19 +2,28 @@ package com.lovetropics.minigames.common.core.game.behavior.instances.action;
 
 import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
+import com.lovetropics.minigames.common.core.game.behavior.action.ActionSubjects;
 import com.lovetropics.minigames.common.core.game.behavior.action.GameActionList;
 import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
 import com.lovetropics.minigames.common.core.game.behavior.event.GameActionEvents;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.ExtraCodecs;
+import net.minecraft.util.context.ContextMap;
 import net.minecraft.util.valueproviders.IntProvider;
+
+import java.util.Optional;
 
 public record DelayedAction(
 		IntProvider delay,
+		Optional<Integer> initialDelay,
+		int repetitions,
 		GameActionList actions
 ) implements IGameBehavior {
 	public static final MapCodec<DelayedAction> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
 			IntProvider.POSITIVE_CODEC.fieldOf("delay").forGetter(b -> b.delay),
+			ExtraCodecs.NON_NEGATIVE_INT.optionalFieldOf("initial_delay").forGetter(b -> b.initialDelay),
+			ExtraCodecs.POSITIVE_INT.optionalFieldOf("repetitions", 1).forGetter(b -> b.repetitions),
 			GameActionList.MAP_CODEC.forGetter(c -> c.actions)
 	).apply(i, DelayedAction::new));
 
@@ -23,11 +32,22 @@ public record DelayedAction(
 		actions.register(game, events);
 
 		events.listen(GameActionEvents.APPLY, (context, targets) -> {
-			int ticks = delay.sample(game.random());
-			game.scheduler().runAfterTicks(ticks, () ->
-					actions.apply(game, context, targets)
-			);
+			schedule(game, context, targets, 0);
 			return true;
+		});
+	}
+
+	private void schedule(IGamePhase game, ContextMap context, ActionSubjects<?> targets, int repeatedCount) {
+		if (repeatedCount >= repetitions) {
+			return;
+		}
+		int ticks = delay.sample(game.random());
+		if (initialDelay.isPresent() && repeatedCount == 0) {
+			ticks = initialDelay.get();
+		}
+		game.scheduler().runAfterTicks(ticks, () -> {
+			actions.apply(game, context, targets);
+			schedule(game, context, targets, repeatedCount + 1);
 		});
 	}
 }
