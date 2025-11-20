9dc29cb79e5c4e05d09157053462ffc9acae0f78
Gegy <me@gegy.dev>
2025-11-20 10:22:22 +0100

    Allow running additional actions on winners/losers from podium

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/action/SetupPodiumAction.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/action/SetupPodiumAction.java
@@ -4,6 +4,8 @@ import com.lovetropics.lib.BlockBox;
 import com.lovetropics.minigames.common.core.game.GameException;
 import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
+import com.lovetropics.minigames.common.core.game.behavior.action.ActionSubjects;
+import com.lovetropics.minigames.common.core.game.behavior.action.GameActionList;
 import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
 import com.lovetropics.minigames.common.core.game.behavior.event.GameActionEvents;
 import com.lovetropics.minigames.common.core.game.behavior.instances.PositionPlayersBehavior;
@@ -16,21 +18,31 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.ExtraCodecs;
+import net.minecraft.util.context.ContextMap;
 
 import java.util.List;
 import java.util.Set;
 
 public record SetupPodiumAction(
 		List<String> winnerRegions,
-		String loserRegion
+		List<GameActionList> winnerActions,
+		String loserRegion,
+		GameActionList loserActions
 ) implements IGameBehavior {
 	public static final MapCodec<SetupPodiumAction> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
 			ExtraCodecs.nonEmptyList(Codec.STRING.listOf()).fieldOf("winner_regions").forGetter(SetupPodiumAction::winnerRegions),
-			Codec.STRING.fieldOf("loser_region").forGetter(SetupPodiumAction::loserRegion)
+			GameActionList.CODEC.listOf().optionalFieldOf("winner_actions", List.of()).forGetter(SetupPodiumAction::winnerActions),
+			Codec.STRING.fieldOf("loser_region").forGetter(SetupPodiumAction::loserRegion),
+			GameActionList.CODEC.optionalFieldOf("loser_actions", GameActionList.EMPTY).forGetter(SetupPodiumAction::loserActions)
 	).apply(i, SetupPodiumAction::new));
 
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) throws GameException {
+		for (GameActionList actions : winnerActions) {
+			actions.register(game, events);
+		}
+		loserActions.register(game, events);
+
 		List<BlockBox> winnerBoxes = winnerRegions.stream().map(game.mapRegions()::getOrThrow).toList();
 		BlockBox loserBox = game.mapRegions().getOrThrow(loserRegion);
 
@@ -54,6 +66,9 @@ public record SetupPodiumAction(
 			int placement = statistics.forPlayer(playerKey).getOr(StatisticKey.PLACEMENT, Integer.MAX_VALUE);
 			if (placement >= 1 && placement <= winnerBoxes.size()) {
 				teleportToRegion(game, player, winnerBoxes.get(placement - 1), loserBox);
+				if (placement <= winnerActions.size()) {
+					winnerActions.get(placement - 1).apply(game, ContextMap.EMPTY, ActionSubjects.ofPlayer(player));
+				}
 				remainingPlayers.remove(player);
 			}
 		}
@@ -61,6 +76,7 @@ public record SetupPodiumAction(
 		for (ServerPlayer player : remainingPlayers) {
 			teleportToRegion(game, player, loserBox, winnerBoxes.getFirst());
 		}
+		loserActions.apply(game, ContextMap.EMPTY, ActionSubjects.ofPlayers(remainingPlayers));
 	}
 
 	private static void teleportToRegion(IGamePhase game, ServerPlayer player, BlockBox region, BlockBox facingRegion) {
