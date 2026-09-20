370c29c812264fb250038a0d9af489cd88ea7a23
Gegy <me@gegy.dev>
2026-09-20 23:31:59 +0200

    Fix: actually bind game lookup

--- src/main/java/org/lovetropics/games/LoveTropics.java
+++ src/main/java/org/lovetropics/games/LoveTropics.java
@@ -53,6 +53,7 @@ import org.lovetropics.games.common.core.data.LoveTropicsAttachments;
 import org.lovetropics.games.common.core.entity.MinigameEntities;
 import org.lovetropics.games.common.core.extension.LimitedSpawnerAttachment;
 import org.lovetropics.games.common.core.game.GameLootModifier;
+import org.lovetropics.games.common.core.game.IGameLookup;
 import org.lovetropics.games.common.core.game.behavior.GameBehaviorTypes;
 import org.lovetropics.games.common.core.game.client_state.GameClientStateTypes;
 import org.lovetropics.games.common.core.game.impl.GameEventDispatcher;
@@ -97,6 +98,7 @@ import net.neoforged.neoforge.event.entity.living.MobSpawnEvent;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.neoforged.neoforge.registries.RegisterEvent;
 import org.lovetropics.games.lobbies.GameLobbyTexts;
+import org.lovetropics.games.lobbies.GamePhaseManager;
 import org.slf4j.Logger;
 
 import java.nio.file.Files;
@@ -209,6 +211,7 @@ public class LoveTropics {
 		modContainer.registerConfig(ModConfig.Type.CLIENT, ConfigLT.CLIENT_CONFIG);
 		modContainer.registerConfig(ModConfig.Type.COMMON, ConfigLT.SERVER_CONFIG);
 
+		IGameLookup.Binder.bind(GamePhaseManager.get());
 		NeoForge.EVENT_BUS.register(new GameEventDispatcher());
 
 		modBus.addListener(this::registerLootModifiers);
